959e9803f119a046fdf2e41908448390ac6a5a18
junior-noel <mbohbemehjunior@mail.com>
2026-09-17 13:48:27 +0100

    created the user folder with all the appropriate files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { UserModule } from './user/user.module.js';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
@@ -14,6 +15,7 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
       appSecret: 'YOUR_APP_SECRET',
       serviceId: 'car-pool',
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
