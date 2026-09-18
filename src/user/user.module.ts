d7b5fbb6170b4a22d7af2599b62bf0340fd30543
junior-noel <mbohbemehjunior@gmail.com>
2026-09-18 11:27:40 +0100

    created the database connection

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller.js';
 import { UserService } from './user.service.js';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity.js';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService]
 })
