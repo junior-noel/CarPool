7ac049db3bbd96fd8f466ab68d453dfffe3596ac
junior-noel <mbohbemehjunior@mail.com>
2026-09-18 17:37:39 +0100

    feat(auth): implement signup logic

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -2,6 +2,7 @@ export class createUserDto{
     firstName: string;
     lastName: string;
     phoneNumber: string;
-    password: string
-
+    password: string;
+    email: string;
+    role: string;
 }
