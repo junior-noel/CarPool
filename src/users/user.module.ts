acef6ab2ca4cd8d9cdd30adeb966c3cd3a360beb
junior-noel <mbohbemehjunior@mail.com>
2026-09-18 14:16:52 +0100

    feat(users): configure users module added comments

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -3,10 +3,13 @@ import { UserController } from './user.controller.js';
 import { UserService } from './user.service.js';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity.js';
-
 @Module({
+  // Registers the User entity's repository.
+  // This allows UsersService to communicate with the users table.
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
+  // Allows other modules, such as AuthModule, to use UsersService.
+  exports: [UserService],
 })
 export class UserModule {}
