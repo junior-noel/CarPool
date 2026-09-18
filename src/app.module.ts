7551d069ecdcf2bc5a49f26ae2106e17797e0de3
junior-noel <mbohbemehjunior@mail.com>
2026-09-18 11:27:40 +0100

    created the database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,22 +7,37 @@ import { AuthModule } from './auth/auth.module.js';
 import { VehicleModule } from './vehicle/vehicle.module.js';
 import { RidesModule } from './rides/rides.module.js';
 import { BookingsModule } from './bookings/bookings.module.js';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity.js';
+
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
 @Module({
   imports: [
-    
     ObserveModule.forRoot({
       appKey: 'YOUR_APP_KEY',
       appSecret: 'YOUR_APP_SECRET',
       serviceId: 'car-pool',
     }),
+
     UserModule,
     AuthModule,
     VehicleModule,
     RidesModule,
     BookingsModule,
+
+    //NestJS module configuration that connect the aplcation to the database
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgresql',
+      database: 'CarPool',
+      entities: [User],
+      synchronize: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    signup() {
+      return {message: 'this is fron authservce signup'};
+  }
+
+    login() {
+       return 'this is fron authservce login';
+  }
+}
