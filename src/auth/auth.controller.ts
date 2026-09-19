0d9b93a987e75fdbf1b50aad3717003303305200
junior-noel <mbohbemehjunior@mail.com>
2026-09-19 15:39:01 +0100

    fix(auth): type authenticated request

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
 
-import { Body, Controller, Post, Get } from '@nestjs/common';
+import { Body, Controller, Post, Get, UseGuards, Req} from '@nestjs/common';
 import { AuthService } from './auth.service.js';
 import { signupDto } from './dto/signup.dto.js';
 import { loginDto } from './dto/login.dto.js';
-import { request } from 'http';
-
+import { JwtAuthGuard } from './guards/jwt-auth.guard.js';
+import type { AuthenticatedRequest } from './interfaces/authenticated-request.interface.js';
 
 @Controller('auth')
 export class AuthController {
+  // Inject AuthService to access authentication methods.
   constructor(private readonly authService: AuthService) {}
 
   // Handles POST requests sent to /auth/signup.
@@ -25,14 +26,14 @@ export class AuthController {
     return this.authService.login(loginDto);
   }
 
-
+  // Handles GET /auth/profile.
+  // JwtAuthGuard runs BEFORE this methodTherefore, only authenticated users can access it..
+ @UseGuards(JwtAuthGuard)
   @Get('profile')
-  getProfile() {
-
+  getProfile(@Req() request: AuthenticatedRequest) {
     return {
-      message: "You are authenticiated",
-      //user: request.user
-    }
-    
+      message: 'You are authenticiated',
+      user: request.user
+    };
   }
 }
