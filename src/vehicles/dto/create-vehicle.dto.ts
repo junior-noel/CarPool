f1e5de4b2bbb4732f3f48cd24299b2e1798ddeb8
junior-noel <mbohbemehjunior@gmail.com>
2026-09-21 09:25:10 +0100

    feat(vehicle): add vehicle creation DTO

---
+++ src/vehicles/dto/create-vehicle.dto.ts
@@ -0,0 +1,30 @@
+import { IsInt, IsNotEmpty, IsString, MIN, Max, Min } from 'class-validator';
+
+export class CreatedVehicleDto {
+    @IsString()
+    @IsNotEmpty()
+    make: string;
+
+    @IsString()
+    @IsNotEmpty()
+    model: string;
+
+    @IsString()
+    @IsNotEmpty()
+    color: string
+
+    @IsInt()
+    @IsNotEmpty()
+    @Min(1900)
+    @Max(2100)
+    year: number;
+
+    @IsString()
+    @IsNotEmpty()
+    licensePlate: string;
+
+    @IsInt()
+    @Min(1)
+    @Max(50)
+    seats: number;
+}
